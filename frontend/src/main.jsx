8491ae42d0bc66ed63e3572b1324c775e577c84b
ray.wu <raywu918python@gmail.com>
2026-09-10 00:25:56 +0800

    Prevent blank screen during date changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
-import { installUiIntegration } from "./uiIntegration.js";
-
-installUiIntegration();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
